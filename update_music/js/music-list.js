c330b6e319e52699e1bc3908f0da89153278575e
HUSEN ALGIFARI <93850135+Husen844algifari@users.noreply.github.com>
2022-09-04 14:25:32 +0800

    Add files via upload

---
+++ update_music/js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Casablanca",
+    artist: "Audio Library",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Forever Young",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "On My Mind",
+    artist: "Audio Library",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Marvin Gaye",
+    artist: "Audio Library",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Peace Be Upon You",
+    artist: "Audio Library",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Until I Found You",
+    artist: "Audio Library",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
